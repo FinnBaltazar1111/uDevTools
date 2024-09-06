03fea88ac6387a0cc682efe1d906e370c847e04d
s0urce-c0de <email.kanavg@gmail.com>
2024-09-05 17:21:04 -0700

    overhaul

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ To run:
 2. Go to `chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/dashboard.html`
 3. Scroll down and check *I am an advanced user*
 
-   ![image](https://github.com/user-attachments/assets/5910353d-dddc-4661-8278-ef055565a115) -> ![image](https://github.com/user-attachments/assets/a0501767-a2ba-46a0-94f0-7d129b9562ab)
+   ![](https://github.com/user-attachments/assets/5910353d-dddc-4661-8278-ef055565a115) -> ![](https://github.com/user-attachments/assets/a0501767-a2ba-46a0-94f0-7d129b9562ab)
 4. Click the <img alt="settings icon" src="https://github.com/user-attachments/assets/1de7481e-d631-42d1-9558-7bf1c22ae253" height="10"> right next to the check box (this will bring you to chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/advanced-settings.html
-5. On the last line next to `userResourcesLocation`, change `unset` to `https://raw.githubusercontent.com/FinnBaltazar1111/uDevTools/main/uDevTools.js` and click *Apply Changes*. If it is already set you can change it from something like `userResourcesLocation https://raw.githubusercontent.com/3kh0/ext-remover/main/ublockExec.js` (uRun) to `userResourcesLocation https://raw.githubusercontent.com/3kh0/ext-remover/main/ublockExec.js https://raw.githubusercontent.com/FinnBaltazar1111/uDevTools/main/uDevTools.js` (uRun + uDevtools)
+5. On the last line next to `userResourcesLocation`, change `unset` to `https://raw.githubusercontent.com/FinnBaltazar1111/uDevTools/main/uDevTools.min.js` and click *Apply Changes*. If it is already set you can change it from something like `userResourcesLocation https://raw.githubusercontent.com/3kh0/ext-remover/main/ublockExec.js` (uRun) to `userResourcesLocation https://raw.githubusercontent.com/3kh0/ext-remover/main/ublockExec.js https://raw.githubusercontent.com/FinnBaltazar1111/uDevTools/main/uDevTools.min.js` (uRun + uDevtools)
 6. Click *Apply Changes*
 7. Go to chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/dashboard.html#1p-filters.html
-8. Inside the edit box, copy-paste this: `*##+js(uDevTools.js)`, then click *Apply Changes*.
+8. Inside the edit box, copy-paste this: `*##+js(chii.js)` in a new line, then click *Apply Changes*.
 
 And you're done! Note that this might not work on some sites due to additional protections.
 
@@ -31,3 +31,32 @@ Websites known not to work:
 * Google Docs, Slides, Drive, Sheets, Jamboard, Meet, etc.
 * Discord
 * some more sites.
+
+# uEruda
+A version of uDevTools that works on a lot more sites due to CSP only disabling frames and scripts, but this is being run directly. Quite literally grab eruda from [jsdelivr](https://cdn.jsdelivr.net/npm/eruda@latest/eruda.min.js), put it in a scriptlet, run it. Steps to install (can be installed allong side uDevTools):
+
+1. Install [uBlock Origin from Chrome Web Store](https://chromewebstore.google.com/detail/cjpalhdlnbpafiamejdnhcphjbkeiagm) (if it's blocked womp womp)
+2. Go to `chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/dashboard.html`
+3. Scroll down and check *I am an advanced user*
+
+   ![](https://github.com/user-attachments/assets/5910353d-dddc-4661-8278-ef055565a115) -> ![](https://github.com/user-attachments/assets/a0501767-a2ba-46a0-94f0-7d129b9562ab)
+4. Click the <img alt="settings icon" src="https://github.com/user-attachments/assets/1de7481e-d631-42d1-9558-7bf1c22ae253" height="10"> right next to the check box (this will bring you to chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/advanced-settings.html
+5. On the last line next to `userResourcesLocation`, change `unset` to `https://raw.githubusercontent.com/FinnBaltazar1111/uDevTools/main/uDevTools.min.js` and click *Apply Changes*. If it is already set you can change it from something like `userResourcesLocation https://raw.githubusercontent.com/3kh0/ext-remover/main/ublockExec.js` (uRun) to `userResourcesLocation https://raw.githubusercontent.com/3kh0/ext-remover/main/ublockExec.js https://raw.githubusercontent.com/FinnBaltazar1111/uDevTools/main/uEruda.js` (uRun + uEruda).
+6. Click *Apply Changes*
+7. Go to chrome-extension://cjpalhdlnbpafiamejdnhcphjbkeiagm/dashboard.html#1p-filters.html
+8. Inside the edit box, copy-paste this: `*##+js(eruda.js)` in a new line, then click *Apply Changes*.
+
+Everything works except Google Docs/Sheets/Drive/etc.
+
+
+# Credits
+* [surunzi](https://github.com/surunzi) or [liriliri](https://github.com/liriliri) for making eruda and chii.
+* Everybody who has contributed and made uBlock Origin a thing.
+* Google, for making a absolutely dumb browser 💀
+* Finn Baltazar for writing the initial code (which all of it has been replaced with a harder to stop version for lazy devs).
+  * GitHub: @FinnBaltazar1111 https://github.com/FinnBaltazar1111
+  * Discord: \@ReznorsRevenge https://discord.com/users/764259980865699842
+  * YouTube Reznor's Revenge https://youtube.com/@ReznorsRevenge1
+* s0urce-c0de, The man behind it all (also the person writing most of this and uDevTools.js and fixing practically everything, from broken injection to togglable shortcut and keybinds.)
+  * GitHub: @s0urce-c0de https://github.com/s0urce-c0de
+  * Discord \@s0urce_c0de https://discord.com/users/947649923342540860
--- uDevTools.js
+++ uDevTools.js
@@ -1,2 +1,150 @@
-/// uDevTools.js
-!async function(){try{let t={url_is_chii:function(t){let e=new URL(t);return"chii.liriliri.io"===e.host&&"/front_end/chii_app.html"===e.pathname}};if(t.get_chii_frame=function(){return[...document.querySelectorAll("iframe[src]")].map(function(t){if(function(){try{return this(HTMLElement.prototype.getAttribute.bind(t,"src")())}catch{try{this(t.src)}catch{return!1}}}.bind(this)())return t}.bind(this))[0]}.bind(t.url_is_chii),t.toggle_chii=function(){let t=this();document.body.style.height="none"==(t.parentNode.style.display="none"===window.getComputedStyle(t.parentNode,null).display?"":"none")?"":document.documentElement.clientHeight-Math.floor(Number(localStorage["chii-embedded-height"]??document.documentElement.clientHeight/2)||100)+"px"}.bind(t.get_chii_frame),!t.url_is_chii(window.location)){let e=document.createElement("script");HTMLElement.prototype.setAttribute.bind(e,"embedded","true")(),HTMLElement.prototype.setAttribute.bind(e,"src","https://chii.liriliri.io/target.js")(),e.addEventListener("load",t.toggle_chii),"loading"===document.readyState?document.addEventListener("DOMContentLoaded",function(){document.head.appendChild(this)}.bind(e)):document.body.appendChild(e),document.addEventListener("keydown",function(t){t.ctrlKey&&t.altKey&&"i"===t.key&&this()}.bind(t.toggle_chii))}}catch{}}();
+/* 
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <https://unlicense.org/>
+ */
+
+/// chii.js
+// This is in an encapsulated scope so that no program can modify this easily.
+// The try-catch block is to let other scriptlets run if this scriptlet errors.
+(async function(){
+    try {
+      // We bind the functions so that they can't be modified by programs intercepting calls.
+      // I mean you can modify `Function.prototype.bind` but it
+      // breaks a lot of libraries (and sometimes jQuery too I think)
+      // so it is logical to assume against.
+      let chiimngr = {}
+      chiimngr.url_is_chii =  (function(url){
+        // `window.URL` can be changed but it breaks nearly all URL parsing so I'll assume
+        // they won't change it.
+        let src = new URL(url)
+        return src.host==="chii.liriliri.io" && src.pathname==="/front_end/chii_app.html"
+      }),
+      chiimngr.get_chii_frame = (function(){
+        // Disabling Array.prototype.map would break lots of iteration
+        // code and disabling HTMLElement.prototype.getAttribute would
+        // break any code trying to read any attributes. This could break
+        // jQuery and other libs to that a site doesn't want to break.
+        return [...document.querySelectorAll("iframe[src]")].map(
+          (function(e){
+            if ((function(){
+              try {
+                return this(
+                  // Disabling HTMLElement.prototype.getAttribute would
+                  // break libraries like jQuery that might try to read
+                  // it
+                  HTMLElement.prototype.getAttribute.bind(e,"src")()
+                )
+              } catch{
+                try {
+                  this(e.src)
+                } catch {
+                  // better no devtools than fake devtools a site
+                  // could inject.
+                  return false
+                }
+              }
+            }).bind(this)()){
+              return e
+            }
+          }).bind(this)
+        )[0]
+      }).bind(chiimngr.url_is_chii),
+      chiimngr.toggle_chii = (function(){
+        let chiiframe = this();
+        // Disabling/hiding chii devtools requires more finese than should
+        // be necessary. You shouldn't need to calculate a height.
+        // Also there is a point to where some things just don't need to be spoofed.
+        // You can try to change the height, but it would just be a minor annoyance.
+        document.body.style.height=(
+          chiiframe.parentNode.style.display = window.getComputedStyle(
+            chiiframe.parentNode, 
+            null
+        ).display==="none"?'':"none"
+        )==="none"?"":`${
+          // I believe 100 is the minimum height.
+          // You can smash is lower by resizing the window.
+          document.documentElement.clientHeight - Math.floor(Number(localStorage['chii-embedded-height']??document.documentElement.clientHeight/2) || 100)
+        }px`
+      }).bind(chiimngr.get_chii_frame)
+      //
+      // recursive devtools would be bad since since we inject on page load, not when it asks
+      // us to activate. this would create infinite iframes, so we check if it is not chii.
+      if (!chiimngr.url_is_chii(window.location)){
+        // Disabling createElement would make it so you need extensive
+        // anti-xss checks because you won't have access to apis to set
+        // inner content without document.write (which is somewhat prone to xss)
+        // (Google reCaptcha has angular which lets you use ng-on-error for xss)
+        let chiiscript = document.createElement('script');
+        // Same as before, it is unfeasable to replace
+        HTMLElement.prototype.setAttribute.bind(chiiscript, "embedded", "true")();
+        HTMLElement.prototype.setAttribute.bind(chiiscript, "src", "https://chii.liriliri.io/target.js")();
+        // what is the point of disabling event listeners...
+        // nearly all pages need them. you probably can't disable
+        // them safely and neatly.
+        chiiscript.addEventListener('load', chiimngr.toggle_chii)
+        // You just can't change document.readyState
+        if (document.readyState === "loading") {
+          // document.head.appendChild.bind(chiiscript) maybe
+          // can document.head change?
+          document.addEventListener("DOMContentLoaded", function(){
+            document.head.appendChild(this)
+          }.bind(chiiscript))
+        } else {
+          // disabling appendChild is like removing a way
+          // to add elements to HTML, very hard to code with
+          document.body.appendChild(chiiscript);
+        }
+        document.addEventListener("keydown", (function(e){
+          // CTRL+ALT+I
+          if (e.ctrlKey && e.altKey && e.key==="i"){
+            this()
+          }
+        }).bind(chiimngr.toggle_chii))
+      };
+   } catch{}
+  })()
+  
+  /*
+   * Congratulations! You reached the end of
+   * the non-minified source code. Give yourself
+   * a pat on the back. Now here is how you fix it.
+   * SET A CSP HEADERS. Quite literally. 
+   * frame-src: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/frame-src
+   * script-src: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src
+   * Remember, work smarter not harder. ;)
+   * Credits:
+   * Finn Baltazar wrote the basic script for injecting chii.
+   * Discord: @ReznorsRevenge https://discord.com/users/764259980865699842
+   * GitHub: @FinnBaltazer1111 https://github.com/FinnBaltazar1111
+   * YouTube: Reznor's Revenge https://www.youtube.com/@ReznorsRevenge1
+   * s0urce-c0de (me) wrote the entire functionality (including eventlistener
+   * activation, toggleable devtools, extra help.)
+   * also all these comments.
+   * Discord: @s0urce_c0de https://discord.com/users/947649923342540860
+   * GitHub: @s0urce-c0de https://github.com/s0urce-c0de
+   * liriliri/surunzi (https://github.com/liriliri)
+   * uBlock Origin team
+   */
+  
